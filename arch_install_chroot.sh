624d35daa78762e877016856bbfa8934b7e7f2ae
Trigex <trigexss@gmail.com>
2019-04-08 20:47:37 -0700

    that should be everything

--- arch_install_chroot.sh
+++ arch_install_chroot.sh
@@ -3,8 +3,6 @@
 
 waitForUserInput() { read -n 1 -s -r -p "Press any key to continue"; }
 
-hostsTemplate = ""
-
 installer() {
     ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
     hwclock --systohc
@@ -14,7 +12,7 @@ installer() {
     locale-gen
     echo "LANG=en_US.UTF-8" > /etc/locale.conf
     read -p "Enter hostname: " hostname
-    echo $hostname > /etc/hostname
+    echo "$hostname" > /etc/hostname
     # puts hostname in hosts template 
     sed -e "s/\${i}/1/" -e "s/\${hostname}/dog/" -e "s/\${i}/1/" -e "s/\${hostname}/dog/" hosts_template > /etc/hosts
     echo "Set root password"
@@ -24,6 +22,8 @@ installer() {
     grub-install --target=i386-pc /dev/sda
     # generate bootloader config
     grub-mkconfig -o /boot/grub/grub.cfg
+    # enable networkmanager
+    systemctl enable NetworkManager.service
     echo "Install completed! Exiting chroot and rebooting..."
     waitForUserInput
     exit
---
+++ arch_install_general.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Post main install, general setup. This is the part where it gets very specific to me! Again, modify as you please lol
+
+setup() {
+    echo "Post install setup!!!"
+    echo "Adding new user..."
+    read -p "Enter username: " username
+    useradd -m -G wheel $username
+    passwd $username
+    EDITOR=vim visudo
+    vim /etc/pacman.conf
+    pacman -Sy
+    pacman -S xf86-video-vesa mesa
+    pacman -S nvidia lib32-nvidia-libgl nvidia-settings
+    pacman -S xorg-server xorg-xinit
+    echo "That should be enough, install whatever DE you want"
+}
+
+setup
